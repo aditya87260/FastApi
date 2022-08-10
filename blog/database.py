52c91e4db1dfd7007a073382f0d39f5760843190
aditya srivastava <adityasrivastava.87260@gmail.com>
2022-08-10 14:46:47 +0530

    ok

--- blog/database.py
+++ blog/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base,sessionmaker
 
 
-SQLALCHAMY_DATABASE_URL= 'sqlite:///blog.db'
+SQLALCHAMY_DATABASE_URL= 'sqlite:///./blog.db'
 
 engine= create_engine(SQLALCHAMY_DATABASE_URL, connect_args={'check_same_thread':False})
 
